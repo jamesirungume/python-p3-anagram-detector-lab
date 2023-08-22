fcb61b22e0c8441554464e2d30267b5c8359f1f5
Your Name <jymohcooler@gmail.com>
2023-08-22 13:07:49 +0300

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self,listItems):
+        similar=[]
+        for item in listItems:
+            if sorted(self.word.lower()) == sorted(item.lower()) and self.word.lower() != item.lower():
+                similar.append(item)
+        return similar
